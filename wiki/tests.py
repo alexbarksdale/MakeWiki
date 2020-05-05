ac4f6e6ede31f85a57d2b68e602df6f3ed552a1c
alexbarksdale <ale.barksdale@gmail.com>
2020-05-04 23:37:13 -0500

    [Add] Detail tests

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from wiki.models import Page
 
-# Create your tests here.
+
+class ListViewTests(TestCase):
+    def test_detail_page(self):
+        # Instance of user to test the pages
+        user = User.objects.create()
+
+        # Create a test detail page
+        Page.objects.create(title="Test", content="test content", author=user)
+
+        # Making a GET request to the home page
+        res = self.client.get('/')
+
+        # Very a 200 response
+        self.assertEqual(res.status_code, 200)
+        result = res.context['pages']
+
+        # Check if we got out test page
+        self.assertQuerysetEqual(
+            result,
+            ['<Page: Test>']
+        )
--- wiki/urls.py
+++ wiki/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from wiki.views import PageListView, PageDetailView, PageCreateView
 
-
 urlpatterns = [
     path('', PageListView.as_view(), name='wiki-list-page'),
     path('create/', PageCreateView.as_view(), name='wiki-create-page'),
     path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
-
 ]
