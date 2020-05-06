1bdd331b4ac7f8615af1c11b1d1bb713041135a0
alexbarksdale <ale.barksdale@gmail.com>
2020-05-05 20:11:27 -0500

    [Add] Create test

--- wiki/tests.py
+++ wiki/tests.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from wiki.models import Page
 
 
-class ListViewTests(TestCase):
+class WikiTests(TestCase):
     def test_detail_page(self):
         # Instance of user to test the pages
         user = User.objects.create()
@@ -30,19 +30,18 @@ class ListViewTests(TestCase):
 
         # Post data to be sent via the form
         post_data = {
-            'title': 'Test2',
-            'content': 'test2 content',
+            'title': 'Test',
+            'content': 'test content',
             'author': user.id
         }
 
         # Request to create a post
         res = self.client.post('/create/', post_data=post_data)
 
-        # Check if we got a 302
-        self.assertEqual(res.status_code, 302)
+        self.assertEqual(res.status_code, 200)
 
         # Get object to test
-        page_object = Page.objects.get(title='Test2')
+        page_object = Page.objects.get(title='Test')
 
         # Check that the page object was created in the test db
-        self.assertEqual(item.title, 'Test2')
+        self.assertEqual(item.title, 'Test')
